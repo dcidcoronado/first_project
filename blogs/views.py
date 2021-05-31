6f15da05c24f283826a439fd377d2e584c1a357f
dcidcoronado <daniel.cid.coronado@gmail.com>
2021-05-31 17:17:33 -0400

    primer commit

---
+++ blogs/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, HttpResponse, redirect
+def root(request):
+    return redirect ("/blogs")
+
+
+def index(request):
+    respuesta = "placeholder para luego mostrar una lista de todos los blogs"
+    return HttpResponse(respuesta)
+
+
+def new(request):
+    respuesta = "placeholder para mostrar un nuevo formulario para crear un nuevo blog"
+    return HttpResponse(respuesta)
+
+
+def create(request):
+    return redirect ("/")
+
+
+def show(request, number):
+    respuesta = f"placeholder para mostrar el blog numero: {number}"
+    return HttpResponse(respuesta)
+
+
+def edit(request, number):
+    respuesta = f"placeholder para editar el blog {number}"
+    return HttpResponse(respuesta)
+
+def destroy(request, number):
+    return redirect ("/blogs")
+
+
+# def edad(request, name, age):
+#     agemayor = str(age + 1)
+#     respuesta = name + " tiene " + agemayor + " años"
+#     return HttpResponse(respuesta)
+
+# Create your views here.
